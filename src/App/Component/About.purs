652d0e2e54485274c80069f80aa5c1ebf1dd93bc
Theerawat Kiatdarakun <tkiat@tutanota.com>
2021-12-10 18:21:08 +0700

    UPDATE

--- src/App/Component/About.purs
+++ src/App/Component/About.purs
@@ -14,8 +14,13 @@ about =
       , R.a {href: "https://tkiat.github.io", children: [R.text "tkiat.github.io"]}
       ]
     , R.h1_ [R.text "About This Blog"]
-    , R.p_ [R.text "The theme of this blog is freedom, anything that reasonably (by my standard) respects others' freedom. Articles usually favor free software and sustainable products and lifestyle. All posts are not associated with anyone or any organization unless explicitly say so."]
-    , R.p_ [R.text "I intend to write everything here and post a few of them to other (more) popular public sites later. I made this website from scratch (using Purescript, SCSS, and React.js) mainly because I wanted to personalize my blog and decouple the article metadata from the Markdown content."]
+    , R.p_ [R.text "Despite having advanced technologies and knowledge nowadays, we still don't respect others' freedom as much as I wish we do. The amount of killing in the meat industry and subtle violations of human rights are staggering."]
+    , R.p_ [R.text "This is why I write this blog to promote freedom. The content favors free software, sustainable products, and lifestyle. All posts are not associated with anyone or any organization unless explicitly say so."]
+    , R.p_ [
+        R.text "I made this website from scratch (using Purescript, SCSS, and React.js) mainly because I wanted to personalize my blog. The source code is on "
+      , R.a {href: "https://github.com/tkiat/a-freedom-blog", children: [R.text "GitHub"]}
+      , R.text "."
+      ]
     , R.h1_ [R.text "Credits"]
     , R.p_ [
         R.text "Favicon from "
